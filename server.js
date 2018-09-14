e73f0f7b01587e8d3b6cd722060d5460a22ee1c9
Ishan <ishan1608@gmail.com>
2018-09-15 01:25:46 +0530

    redirecting to display url

--- server.js
+++ server.js
@@ -68,9 +68,8 @@ http.createServer(function (req, res) {
                     var objFile = files['obj_file'];
                     fs.createReadStream(objFile.path).pipe(fs.createWriteStream('public/uploads/' + objFile.name));
 
-                    res.writeHead(200, {'content-type': 'text/plain'});
-                    res.write('received upload:\n\n');
-                    res.end(util.inspect({fields: fields, files: files}));
+                    res.writeHead(302, {'Location': '/display/?file=' + objFile.name});
+                    res.end();
                 });
                 break;
             default:
